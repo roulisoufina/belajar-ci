a6de706ca2f6dd2802d0763ea6edf992520451c5
Rouly Soufina <roulykecedonk@gmail.com>
2018-04-30 15:38:38 +0700

    template projet

---
+++ src/main/java/siahaan/com/example/belajarci/BelajarCiApplication.java
@@ -0,0 +1,12 @@
+package siahaan.com.example.belajarci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarCiApplication.class, args);
+	}
+}
